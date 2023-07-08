8a52183ab38a1a4ca0e9fbd3cb5ed760573eb631
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-07-08 14:54:15 +0530

    Removed unwanted comments in code

--- maxmin/src/maxmin/MaxndMin.java
+++ maxmin/src/maxmin/MaxndMin.java
@@ -15,28 +15,24 @@ public static void main(String[] args) {
 	}
 	int min = arr[0];
 	int max =arr[0];
-//	int maxi;
-//	int mini;
+	
 	for(int i=0;i<=arr.length-1;i++)
 	{
 		if (max < arr[i])
 		{
 			max = arr[i];
-//			maxi =i;
+
 		}
 		
 		if (min > arr[i])
 		{
 			min = arr[i];
-//			mini =i;
+
 		}
 		
 	}
 	
 	System.out.println("Max is;"+max);
 	System.out.println("Min is;"+min);
-//	System.out.println("pos is;"+maxi);
-//	System.out.println("pos is;"+mini);
-//	System.out.println("positio is:"max[i]);
 	}
 }
